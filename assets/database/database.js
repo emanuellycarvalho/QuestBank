1ed6e9cdc3e082c4c8e8a585aa3c3f56bc154dd0
Emanuelly Carvalho <emanuellyvcarv@gmail.com>
2024-05-27 17:44:59 -0300

    add: enriquecendo banco de dados

--- assets/database/database.js
+++ assets/database/database.js
@@ -89,6 +89,14 @@ function populateThemes() {
 
     themesStore.add({ name: 'Geografia' });
     themesStore.add({ name: 'Matemática' });
+    themesStore.add({ name: 'História' });
+    themesStore.add({ name: 'Biologia' });
+    themesStore.add({ name: 'Física' });
+    themesStore.add({ name: 'Química' });
+    themesStore.add({ name: 'Literatura' });
+    themesStore.add({ name: 'Artes' });
+    themesStore.add({ name: 'Filosofia' });
+    themesStore.add({ name: 'Sociologia' });
     console.log('Tabela de temas povoada.');
 }
 
@@ -98,6 +106,14 @@ function populateQuestions() {
 
     questionsStore.add({ question: 'Qual a capital do Brasil?', theme_id: 1, isPublic: false, user_id: 1 });
     questionsStore.add({ question: 'Quanto é 2 + 2?', theme_id: 2, isPublic: true, user_id: 1 });
+    questionsStore.add({ question: 'Quem descobriu o Brasil?', theme_id: 3, isPublic: true, user_id: 1 });
+    questionsStore.add({ question: 'Qual é a fórmula da água?', theme_id: 4, isPublic: true, user_id: 1 });
+    questionsStore.add({ question: 'O que é a Terceira Lei de Newton?', theme_id: 5, isPublic: true, user_id: 1 });
+    questionsStore.add({ question: 'Quem escreveu Dom Casmurro?', theme_id: 6, isPublic: true, user_id: 1 });
+    questionsStore.add({ question: 'Qual a capital da França?', theme_id: 7, isPublic: true, user_id: 1 });
+    questionsStore.add({ question: 'Quem pintou a Mona Lisa?', theme_id: 8, isPublic: true, user_id: 1 });
+    questionsStore.add({ question: 'O que é a Democracia?', theme_id: 9, isPublic: true, user_id: 1 });
+    questionsStore.add({ question: 'O que é a Teoria da Relatividade?', theme_id: 10, isPublic: true, user_id: 1 });
     console.log('Tabela de questões povoada.');
 }
 
@@ -105,8 +121,11 @@ function populateUsers() {
     var transaction = db.transaction(['users'], 'readwrite');
     var usersStore = transaction.objectStore('users');
 
-    usersStore.add({ name: 'Professor', email: 'professor@example.com', password: '123456', type: 1 }); // type 1 para professor
-    usersStore.add({ name: 'Aluno', email: 'aluno@example.com', password: '123456', type: 2 }); // type 2 para aluno
+    usersStore.add({ name: 'Professor1', email: 'professor1@example.com', password: '123456', type: 1 });
+    usersStore.add({ name: 'Professor2', email: 'professor2@example.com', password: '123456', type: 1 });
+    usersStore.add({ name: 'Professor3', email: 'professor3@example.com', password: '123456', type: 1 });
+    usersStore.add({ name: 'Aluno1', email: 'aluno1@example.com', password: '123456', type: 0 });
+    usersStore.add({ name: 'Aluno2', email: 'aluno2@example.com', password: '123456', type: 0 });
     console.log('Tabela de usuários povoada.');
 }
 
@@ -114,7 +133,9 @@ function populateHistory() {
     var transaction = db.transaction(['history'], 'readwrite');
     var historyStore = transaction.objectStore('history');
 
-    historyStore.add({ question_id: 1, user_id: 1, date: '2024-03-25', class: 'TZ', accuracy_average: 65, observations: 'Alunos tiveram dificuldade com esta questão.' });
-    historyStore.add({ question_id: 2, user_id: 2, date: '2024-03-25', class: 'TM', accuracy_average: 90, observations: 'Questão respondida corretamente pela maioria dos alunos.' });
+    for (let i = 1; i <= 30; i++) {
+        historyStore.add({ question_id: Math.floor(Math.random() * 10) + 1, user_id: Math.floor(Math.random() * 5) + 1, date: `2024-03-${i < 10 ? '0' + i : i}`, class: `T${i % 5}`, accuracy_average: Math.floor(Math.random() * 100), observations: `Observações para o histórico ${i}` });
+    }
+
     console.log('Tabela de histórico de uso povoada.');
 }
